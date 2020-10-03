0b668a192b01c6181c4a17e79ec805fe93f58808
Quentin Pietro Milana <quentin.milana@hotmail.fr>
2020-10-03 15:16:01 +0200

    New Organisation

---
+++ Test/Test Project/Assets/Features/Quentin/QuentinDev/CamController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    [SerializeField] private CamDataBehavior camDataBehavior;
+    [SerializeField] private Rotate rotate;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rotate.DoRotate(camDataBehavior.camData.speedRotation);
+        }
+    }
+}
---
+++ Test/Test Project/Assets/Features/Quentin/QuentinDev/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    
+
+    public void DoRotate(int value)
+    {
+        Debug.Log(value);
+    }
+}

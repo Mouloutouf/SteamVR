e104c274938e8a40907479289f54378d52c2ec9f
Maxence <maxdurlon@gmail.com>
2020-10-04 16:29:42 +0200

    Pinch & Zoom Movement Mobile

---
+++ Test/Test Project/Assets/Features/Maxence-Dev/Testing Room/ElectricalConnection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mobile_Dev
+{
+    public class ElectricalConnection : MonoBehaviour
+    {
+        public Sprite camCurrent;
+        public Sprite trapCurrent;
+    }
+}

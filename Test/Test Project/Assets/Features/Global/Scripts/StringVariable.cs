ca7e27298b741f3c1a96c742e4224ec428dec85c
QuentinPietro <quentin.milana@hotmail.fr>
2020-10-05 19:48:42 +0200

    Network Improvements

---
+++ Test/Test Project/Assets/Features/Global/Scripts/StringVariable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GamePlay
+{
+    [CreateAssetMenu]
+    public class StringVariable : ScriptableObject
+    {
+        public string Value;
+
+        public void SetValue(string value) => Value = value;
+    }
+}
+

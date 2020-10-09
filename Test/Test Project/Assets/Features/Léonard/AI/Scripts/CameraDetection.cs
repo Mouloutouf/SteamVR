d977263eff71175cfd4ad5c6b07c15a4db8eee85
Léonard <epoc4oftime@gmail.com>
2020-10-09 12:31:29 +0200

    Converted AI variables to Custom Atoms

--- Test/Test Project/Assets/Features/Léonard/AI/Scripts/CameraDetection.cs
+++ Test/Test Project/Assets/Features/Léonard/AI/Scripts/CameraDetection.cs
@@ -24,17 +24,17 @@ public class CameraDetection : DetectionData
             float angleDifference = Vector3.Angle(targetDir, transform.forward);
 
             // returns true if the angle between the two is less than the detection angle
-            detectingPlayer = angleDifference <= detectionAngle ? true : false;
+            detectingPlayer = angleDifference <= detectionAngle.Value ? true : false;
 
             while (detectingPlayer)
             {
                 Debug.Log("I might've seen something");
 
-                yield return new WaitForSeconds(detectionSpeed);
+                yield return new WaitForSeconds(detectionSpeed.Value);
 
                 float angleDifference2 = Vector3.Angle(targetDir, transform.forward);
                 
-                lockingPlayer = angleDifference2 <= detectionAngle ? true : false;
+                lockingPlayer = angleDifference2 <= detectionAngle.Value * 0.5f ? true : false; // divide the angle by half because it is replicated on either sides of transform.forward
 
                 if (lockingPlayer == true)
                 {

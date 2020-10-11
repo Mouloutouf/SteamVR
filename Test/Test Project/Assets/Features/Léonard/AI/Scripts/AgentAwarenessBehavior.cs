f3db2467d724f8ffe12598d9aa8eab333564f304
Léonard Gombert <epoc4oftime@gmail.com>
2020-10-11 15:03:35 +0200

    Added Debug Information for Agent and Camera Ranges

--- Test/Test Project/Assets/Features/Léonard/AI/Scripts/AgentAwarenessBehavior.cs
+++ Test/Test Project/Assets/Features/Léonard/AI/Scripts/AgentAwarenessBehavior.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Runtime.Remoting.Messaging;
 using UnityEngine;
 
 namespace Gameplay.VR
@@ -6,6 +7,7 @@ namespace Gameplay.VR
     public class AgentAwarenessBehavior : AgentAwarenessData
     {
         private bool detectingPlayer;
+        RaycastHit hitInfo;
 
         // Start is called before the first frame update
         void Start()
@@ -21,21 +23,25 @@ namespace Gameplay.VR
         {
             while (true)
             {
+                Vector3 forward = transform.forward;
+                forward.y = 0;
+                Debug.DrawRay(transform.localPosition, forward * detectionRange.Value);
+
                 if (detectingPlayer)
                 {
                     yield return new WaitForSeconds(detectionSpeed.Value);
-                    
+
                     if (Vector3.Angle(target.transform.localPosition - transform.localPosition, transform.forward) <= myDetectionAngle)
                     {
                         Debug.Log("I have detected the player");
+                        Debug.DrawLine(transform.localPosition, target.transform.localPosition, Color.green);
                         detectedPlayer.Raise();
-                        break; // exit the coroutine
+                        break;
                     }
-
-                    else yield return null;
                 }
 
-                // check if player is within the camera's "depth" range
+
+                // check if player is within the AI's "depth" range
                 else if (Vector3.Distance(new Vector3(target.transform.localPosition.x, 0, target.transform.localPosition.z), new Vector3(transform.localPosition.x, 0, transform.localPosition.z)) < detectionRange.Value)
                 {
                     Vector3 targetDir = target.transform.localPosition - transform.localPosition;
@@ -47,8 +53,14 @@ namespace Gameplay.VR
 
                     if (playerInSight)
                     {
+                        // if there is something between the AI and the player
+                        if (Physics.Linecast(transform.localPosition, target.transform.localPosition, out hitInfo))
+                        {
+                            Debug.Log("I've hit " + hitInfo.collider.gameObject.name);
+                            yield return null;
+                        }
+
                         Debug.Log("Is that the player...?");
-                        Debug.DrawLine(transform.localPosition, target.transform.localPosition, Color.blue);
                         detectingPlayer = true;
                     }
                 }

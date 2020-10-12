19b4f682297e91762d2dddb5c29767a55a9e1b61
QuentinPietro <quentin.milana@hotmail.fr>
2020-10-12 18:09:26 +0200

    Switch Networking

---
+++ Test/Test Project/Assets/Features/Léonard/Ballistix/Scripts/ProjectileBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay.VR
+{
+    public class ProjectileBehaviour : ProjectileData
+    {
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        public void Launch()
+        {
+            transform.localPosition = gunBarrel.localPosition;
+            transform.localRotation = gunBarrel.localRotation;
+
+            rb.AddRelativeForce(Vector3.forward * bulletForce.Value, ForceMode.Impulse);
+
+            StartCoroutine(Lifetime());
+        }
+
+        private IEnumerator Lifetime()
+        {
+            new WaitForSeconds(bulletLifeTime.Value);
+            yield return null;
+        }
+    }
+}

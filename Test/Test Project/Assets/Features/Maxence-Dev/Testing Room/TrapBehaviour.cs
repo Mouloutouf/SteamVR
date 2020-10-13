8e91d3e987ec711b916bb94d92e538e2ed2e9d66
Maxence <maxdurlon@gmail.com>
2020-10-13 18:29:06 +0200

    A l'Arrache Full Flemme

---
+++ Test/Test Project/Assets/Features/Maxence-Dev/Testing Room/TrapBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GamePlay.VR;
+
+public class TrapBehaviour : MonoBehaviour
+{
+    public GameEvent loseEvent;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") loseEvent.Raise();
+    }
+}
